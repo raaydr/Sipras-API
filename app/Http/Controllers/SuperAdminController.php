43ed22bfd9dcd290180a39f7bf78cfd153803683
Reynaldi Dr <dharmawanreynaldi@gmail.com>
2023-05-14 09:31:00 +0800

    penambahan template

---
+++ app/Http/Controllers/SuperAdminController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Rules\MatchOldPassword;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Auth;
+use Redirect;
+use DataTables;
+use DateTime;
+use Carbon\Carbon;
+
+
+class SuperAdminController extends Controller
+{
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     
+    public function welcome()
+    {
+        $title = 'Welcome Admin';
+        
+        
+
+        $now = Carbon::now(); // today
+        $date = Carbon::parse($now)->isoFormat('D MMMM Y');
+        return view('admin.welcome', compact('date'));
+    }
+
+    public function ubah_password(){
+        $title = 'Admin ubah password';
+        
+        
+
+        //return view('admin.ubahpassword', compact('title'));
+    }
+
+    public function change_password(Request $request)
+
+    {
+
+        $request->validate([
+
+            'current_password' => ['required', new MatchOldPassword],
+
+            'new_password' => ['required'],
+
+            'new_confirm_password' => ['same:new_password'],
+
+        ]);
+
+   
+
+        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+
+   
+        //return redirect()->route('admin.ubah.password')->with('berhasil', 'berhasil ubah password');
+        //return redirect()->route('admin.listPendaftar');
+
+    }
+
+}
