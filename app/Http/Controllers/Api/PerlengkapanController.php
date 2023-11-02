3015502447cd5d854c778921efe83bda582259e2
Reynaldi Dr <dharmawanreynaldi@gmail.com>
2023-11-02 18:57:20 +0800

    preparing api (#1)

---
+++ app/Http/Controllers/Api/PerlengkapanController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PerlengkapanController extends Controller
+{
+    public function PerlengkapanEdit()
+    {
+        
+        
+        
+        return view('master.dataPerlengkapan');
+    }
+}
